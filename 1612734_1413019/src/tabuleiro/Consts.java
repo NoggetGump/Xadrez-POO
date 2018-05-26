8a549b953dbb87bb9b00b437e48ce0ecf0cc86ee
SaintLeonar <31524775+SaintLeonar@users.noreply.github.com>
2018-05-26 18:16:13 -0300

    Comentários de auxilio adicionados

--- 1612734_1413019/src/tabuleiro/Consts.java
+++ 1612734_1413019/src/tabuleiro/Consts.java
@@ -1,7 +1,9 @@
 package tabuleiro;
 
-public class Consts 
+public final class Consts 
 {
 	public static final int xyIni = 0;
 	public static final int xyFin = 8;
+	public static final int x = 0; // Posição do x no vetor coordenada v[] -> v[0] = x
+	public static final int y = 1; // Posição do y no vetor coordenada v[] -> v[1] = y
 }
