89bf895362382f3f0f48204ce60dd3456b628d1a
SaintLeonar <31524775+SaintLeonar@users.noreply.github.com>
2018-05-26 18:17:19 -0300

    Tabuleiro em branco para ser retrabalhado

--- 1612734_1413019/src/tabuleiro/Tabuleiro.java
+++ 1612734_1413019/src/tabuleiro/Tabuleiro.java
@@ -1,41 +1,8 @@
 package tabuleiro;
 
-import java.awt.*;
-import javax.swing.*;
-import java.awt.Graphics2D;
+import pecas.Peca;
 
-public class Tabuleiro extends JComponent{
+public class Tabuleiro {
 	
-<<<<<<< HEAD
-	private Object casa[][];
 	
-	private static void janelaPrincipal()
-	{
-		Tabuleiro tabuleiro = new Tabuleiro();
-		
-		JFrame janela = new JFrame ("Xadrez");
-		janela.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		janela.setLayout(null);
-		janela.setLocationRelativeTo(null);
-		janela.pack();
-		janela.setVisible(true);
-		
-	}
-	public void desenhaRetangulo(Graphics g) 
-    {
-
-		Graphics2D retangulo = (Graphics2D) g;
-        super.paintComponent(g);
-        retangulo.setColor(Color.black);   
-        retangulo.drawRect(10,10,100,100);
-
-    }
-	
-	public static void main (String[] args)
-	{
-		janelaPrincipal();
-		
-	}
-=======
->>>>>>> add31c8ecaa1e66018735f309eaecf61348cb276
 }
