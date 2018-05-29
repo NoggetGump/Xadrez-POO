780c8f3af91273a5f4f37a084bad6f96b7259c8f
SaintLeonar <31524775+SaintLeonar@users.noreply.github.com>
2018-05-29 20:24:47 -0300

    GUI_main Atualizada com bugs corrigidos

--- 1612734_1413019/src/jogo_GUI/GUI_main.java
+++ 1612734_1413019/src/jogo_GUI/GUI_main.java
@@ -1,19 +1,15 @@
 package jogo_GUI;
 
 import java.awt.Color;
-import java.awt.Dimension;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
 import java.awt.geom.Rectangle2D;
-import javax.swing.JApplet;
+
+import javax.swing.JComponent;
 import javax.swing.JFrame;
 
-import tabuleiro.Consts;
+public class GUI_main extends JComponent{
 
-public class GUI_main extends JApplet 
-{
-	private static final long serialVersionUID = 9113343285574283276L; // Ignora isso
-	
 	private final int tam = 100;
 	
 	public void paint(Graphics g) 
@@ -21,88 +17,45 @@ public class GUI_main extends JApplet
 	    Graphics2D g2 = (Graphics2D) g;
 	    g2.setPaint(Color.black);
 	   
-	    int i;
-	    
+	    int i, j, ctrl = 2;
 	    int x = 0;
-	    int y = 100;
+	    int y = 0;
 	    
-	    for(i = Consts.xyIni ; i < Consts.xyFin ; i++)
+	    for(i = 0 ; i < 8 ; i++)
 	    {
-	    	g2.draw(new Rectangle2D.Double(x, y*0, tam, tam));
-		    x = x + tam;
-		    g2.fill(new Rectangle2D.Double(x, y*0, tam, tam));
-		    x = x + tam;
-	    }
-	    x = 0;
-	    for(i = Consts.xyIni ; i < Consts.xyFin ; i++)
-	    {	
-	    	g2.fill(new Rectangle2D.Double(x, y*1, tam, tam));
-		    x = x + tam;
-		    g2.draw(new Rectangle2D.Double(x, y*1, tam, tam));
-		    x = x + tam;
-	    }
-	    x = 0;
-	    for(i = Consts.xyIni ; i < Consts.xyFin ; i++)
-	    {
-	    	g2.draw(new Rectangle2D.Double(x, y*2, tam, tam));
-		    x = x + tam;
-		    g2.fill(new Rectangle2D.Double(x, y*2, tam, tam));
-		    x = x + tam;
-	    }
-	    x = 0;
-	    for(i = Consts.xyIni ; i < Consts.xyFin ; i++)
-	    {	
-	    	g2.fill(new Rectangle2D.Double(x, y*3, tam, tam));
-		    x = x + tam;
-		    g2.draw(new Rectangle2D.Double(x, y*3, tam, tam));
-		    x = x + tam;
-	    }
-	    x = 0;
-	    for(i = Consts.xyIni ; i < Consts.xyFin ; i++)
-	    {
-	    	g2.draw(new Rectangle2D.Double(x, y*4, tam, tam));
-		    x = x + tam;
-		    g2.fill(new Rectangle2D.Double(x, y*4, tam, tam));
-		    x = x + tam;
-	    }
-	    x = 0;
-	    for(i = Consts.xyIni ; i < Consts.xyFin ; i++)
-	    {	
-	    	g2.fill(new Rectangle2D.Double(x, y*5, tam, tam));
-		    x = x + tam;
-		    g2.draw(new Rectangle2D.Double(x, y*5, tam, tam));
-		    x = x + tam;
-	    }
-	    x = 0;
-	    for(i = Consts.xyIni ; i < Consts.xyFin ; i++)
-	    {
-	    	g2.draw(new Rectangle2D.Double(x, y*6, tam, tam));
-		    x = x + tam;
-		    g2.fill(new Rectangle2D.Double(x, y*6, tam, tam));
-		    x = x + tam;
-	    }
-	    x = 0;
-	    for(i = Consts.xyIni ; i < Consts.xyFin ; i++)
-	    {	
-	    	g2.fill(new Rectangle2D.Double(x, y*7, tam, tam));
-		    x = x + tam;
-		    g2.draw(new Rectangle2D.Double(x, y*7, tam, tam));
-		    x = x + tam;
-	    }
-	    x = 0;
-	            
+	    	for(j = 0 ; j < 8/2 ; j++)
+	    	{
+	    		if(ctrl % 2 == 0)
+	    		{
+	    			g2.draw(new Rectangle2D.Double(x, y, tam, tam));
+	    			x = x + tam;
+	    		    g2.fill(new Rectangle2D.Double(x, y, tam, tam));
+	    		    x = x + tam;
+	    		}
+	    		else
+	    		{
+	    			g2.fill(new Rectangle2D.Double(x, y, tam, tam));
+	    			x = x + tam;
+	    		    g2.draw(new Rectangle2D.Double(x, y, tam, tam));
+	    		    x = x + tam;
+	    		}
+	    	}
+	    	x = 0;
+	    	ctrl++;
+	    	y = y + tam;
+	    }            
+	}
+	
+	public void inicializaTabuleiro()
+	{
+		JFrame janela = new JFrame("Xadrez");
+	    janela.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	    janela.setSize(816, 839);
+	    janela.getContentPane().add(new GUI_main());
+	    janela.setLocationRelativeTo(null);
+	    janela.setResizable(false);
+	    janela.setVisible(true);
+		System.out.println("Janela inicializada com sucesso!");
 	}
 
-	public void inicializaGUI() {
-	    
-		JFrame f = new JFrame("Xadrez");
-	    JApplet applet = new GUI_main();
-	    f.getContentPane().add("Center", applet);
-	    f.pack();
-	    f.setSize(new Dimension(816, 839)); // Define tamanho da janela (816px , 839px)
-	    f.setLocationRelativeTo(null); // Inicializa janela no meio da tela
-	    f.setLayout(null); // Gerenciador de Layout: null
-	    f.setResizable(false); // Set janela não redimencionável
-	    f.setVisible(true);
-	}
 }
