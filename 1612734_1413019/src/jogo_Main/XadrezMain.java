5b77036bc259362ea668f0e5af91a02d545232eb
SaintLeonar <31524775+SaintLeonar@users.noreply.github.com>
2018-05-29 20:25:18 -0300

    Update XadrezMain.java

--- 1612734_1413019/src/jogo_Main/XadrezMain.java
+++ 1612734_1413019/src/jogo_Main/XadrezMain.java
@@ -7,7 +7,6 @@ public class XadrezMain {
 	public static void main(String[] args) 
 	{
 		GUI_main g = new GUI_main();
-		g.inicializaGUI();
-		System.out.println("Janela inicializada com sucesso!");
+		g.inicializaTabuleiro();
 	}
 }
