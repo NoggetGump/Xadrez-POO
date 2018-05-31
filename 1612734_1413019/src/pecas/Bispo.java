ad67aeba8dd0e320e708b1b9365714b4f66d6ee3
SaintLeonar <31524775+SaintLeonar@users.noreply.github.com>
2018-05-30 22:27:14 -0300

    Bispo Atualizado

--- 1612734_1413019/src/pecas/Bispo.java
+++ 1612734_1413019/src/pecas/Bispo.java
@@ -1,20 +1,14 @@
 package pecas;
 
-import java.util.ArrayList;
+import movimentos.Diagonal;
 
-public class Bispo extends Peca 
+public class Bispo extends Peca
 {
+	Diagonal D = new Diagonal();
+	
 	public Bispo(int x, int y, char cor)
 	{
 		super(x, y, cor);
-	}
-	
-	public ArrayList<int[]> movimentoValido(int[] v)
-	{
-		ArrayList<int[]> AllMoves = new ArrayList<int[]>();	// Todos os movimentos validos para esse tipo de peça
-		
-		AllMoves = Movimentos.allMovDiag(v);
-		
-		return AllMoves;
+		D.allMovDiag(this.v, this.AllMoves);
 	}
 }
