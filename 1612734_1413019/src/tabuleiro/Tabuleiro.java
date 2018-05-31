d380d02bd3e35a4c6fc858993bd42bffed802f84
SaintLeonar <31524775+SaintLeonar@users.noreply.github.com>
2018-05-30 22:35:40 -0300

    Tabuleiro atualizado

--- 1612734_1413019/src/tabuleiro/Tabuleiro.java
+++ 1612734_1413019/src/tabuleiro/Tabuleiro.java
@@ -1,8 +1,29 @@
 package tabuleiro;
 
 import pecas.Peca;
+import vetor.Vet;
 
-public class Tabuleiro {
+import java.util.ArrayList;
+
+public class Tabuleiro 
+{
+	private ArrayList<Casa> tabuleiro = new ArrayList<Casa>();
+	
+	public Tabuleiro() 
+	{
+		for(int i = 0 ; i < 8 ; i++)
+		{
+			for(int j = 0 ; j < 8 ; j++)
+			{
+				tabuleiro.add(new Casa(i,j));
+			}	
+		}
+	}
+	
+	public void printTabuleiro()
+	{
+		
+	}
 	
 	
 }
