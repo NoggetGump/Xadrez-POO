872da8ccbe05d6fdef8473c35ffc5c91788ea525
SaintLeonar <31524775+SaintLeonar@users.noreply.github.com>
2018-05-30 22:31:12 -0300

    Peca atualizada

--- 1612734_1413019/src/pecas/Peca.java
+++ 1612734_1413019/src/pecas/Peca.java
@@ -1,45 +1,31 @@
 package pecas;
 
 import java.util.ArrayList;
+import java.util.function.Consumer;
+import vetor.Vet;
 
-public abstract class Peca {
+public class Peca{
 	/*Variaveis propositalmente sem modificador 
 	* para serem acessada somente dentro do mesmo pacote*/
-	int v[] = new int[2];	//Vetor coordenada x e y.
+	Vet v;					//Vetor coordenada x e y.
 	char cor;				//Cor preta "p" ou branca "b".
-	boolean vivo;			//1 para peça viva e 0 para peça comida.
+	boolean vivo = true;	//Consts.y para peca viva e Consts.x para peca comida.
+	ArrayList<Vet> AllMoves = new ArrayList<Vet>();
 
 	public Peca(int x, int y, char cor) {
-		this.v[0] = x;
-		this.v[1] = y;
+		this.v = new Vet(x,y);
 		this.cor = cor;
 	}
 
-	public int getX() {
-		return v[0];
-	}
-
-	public void setX(int x) {
-		this.v[0] = x;
-	}
-
-	public int getY() {
-		return v[1];
-	}
-
-	public void setY(int y) {
-		this.v[1] = y;
-	}
-
-	public void setv(int x, int y) {
-		this.v[0] = x;
-		this.v[1] = y;
-	}
-
-	public char getCor() 
-	{
+	public char getCor() {
 		return this.cor;
 	}
-
-	public abstract ArrayList<int[]> movimentoValido(int[] v);
+    public Consumer<Vet> printMoves = (v) -> {
+        System.out.print("(" +v.getX() + "," + v.getY() + ")" + "\t");
+    };
+	
+	public void printAllMov()
+	{
+		this.AllMoves.forEach(printMoves);
+	}
 }
