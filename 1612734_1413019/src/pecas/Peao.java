4bf93a6fe15c0d8f3c03eb403f338add833fe7fd
SaintLeonar <31524775+SaintLeonar@users.noreply.github.com>
2018-05-30 22:30:24 -0300

    Peao Atualizado

--- 1612734_1413019/src/pecas/Peao.java
+++ 1612734_1413019/src/pecas/Peao.java
@@ -1,9 +1,17 @@
 package pecas;
 
-public class Peao extends Peca
-{
+import movimentos.Vertical;
+import vetor.Vet;
+
+public class Peao extends Peca{
+	
+	Vertical V = new Vertical();
+	
 	public Peao(int x, int y, char cor)
 	{
 		super(x, y, cor);
+		Vet Temp = new Vet(v);
+
+		V.addIfTrue(new Vet(Temp), Temp, AllMoves, true);
 	}
 }
