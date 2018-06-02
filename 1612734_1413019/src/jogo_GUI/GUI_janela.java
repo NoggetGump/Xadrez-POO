105709e292abd2970fab738e3c39ac8374735e3c
SaintLeonar <31524775+SaintLeonar@users.noreply.github.com>
2018-06-02 17:56:32 -0300

    GUI_janela criada + mouseClicked

---
+++ 1612734_1413019/src/jogo_GUI/GUI_janela.java
@@ -0,0 +1,33 @@
+package jogo_GUI;
+
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import javax.swing.ImageIcon;
+import javax.swing.JFrame;
+
+public class GUI_janela extends JFrame {
+
+		public GUI_janela()
+		{
+			addMouseListener(new MouseAdapter() 
+			{
+				public void mouseClicked(MouseEvent e)
+				{
+					int x = e.getX();
+					int y = e.getY();
+					System.out.println("Coordenada clicada: ( " + x + " , " + y + " )");
+				}
+			});
+			
+			ImageIcon img = new ImageIcon("Assets\\Chess_Icon2.png");
+			this.setIconImage(img.getImage());
+			this.setTitle("Xadrez");
+		    setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		    setSize(816, 839);
+		    getContentPane().add(new GUI_main());
+		    setLocationRelativeTo(null);
+		    setResizable(false);
+		    setVisible(true);
+			System.out.println("Janela inicializada com sucesso!");
+		}
+}
