8b44e7cae13188f764c755ec0b2173a160d2080d
SaintLeonar <31524775+SaintLeonar@users.noreply.github.com>
2018-06-02 17:57:12 -0300

    GUI_main atualizada

--- 1612734_1413019/src/jogo_GUI/GUI_main.java
+++ 1612734_1413019/src/jogo_GUI/GUI_main.java
@@ -3,238 +3,68 @@ package jogo_GUI;
 import java.awt.Color;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
-import java.awt.Image;
 import java.awt.Toolkit;
 import java.awt.geom.Rectangle2D;
-
 import javax.swing.JComponent;
-import javax.swing.JFrame;
+import pecas.*;
 
 public class GUI_main extends JComponent{
 
 	private final int tam = 100;
-	
-	public void paint(Graphics g) 
+	private final int tamTab = 8 * tam;
+	private static Peca peca;
+
+	public void paintPeca(Peca t, Graphics2D g2) 
+	{
+	    g2.drawImage(Toolkit.getDefaultToolkit().getImage(t.imgPeca()), t.convCoorX(), t.convCoorY(), this);
+	    g2.finalize();
+	}
+
+	public void paint(Graphics g)
 	{
 	    Graphics2D g2 = (Graphics2D) g;
 	    g2.setPaint(Color.black);
-	   
-	    int i, j, x = 0, y = 0, ctrl = 2;
+
+	    int x = 0, y = 0;
 	    
-	    for(i = 0 ; i < 8 ; i++)
+	    while(y<tamTab)
 	    {
-	    	for(j = 0 ; j < 8/2 ; j++)
+	    	if(y%200 == 0) 
 	    	{
-	    		if(ctrl % 2 == 0)
-	    		{
-	    			g2.draw(new Rectangle2D.Double(x, y, tam, tam));
-	    			x = x + tam;
-	    		    g2.fill(new Rectangle2D.Double(x, y, tam, tam));
-	    		    x = x + tam;
-	    		}
-	    		else
-	    		{
-	    			g2.fill(new Rectangle2D.Double(x, y, tam, tam));
-	    			x = x + tam;
-	    		    g2.draw(new Rectangle2D.Double(x, y, tam, tam));
-	    		    x = x + tam;
-	    		}
+	    	g2.draw(new Rectangle2D.Double(x, y, tam, tam));
+	    	x = x + tam;
+	    	g2.fill(new Rectangle2D.Double(x, y, tam, tam));
+	    	x = x + tam;
 	    	}
-	    	x = 0;
-	    	ctrl++;
-	    	y = y + tam;
+	    	else
+	    	{
+		    	g2.fill(new Rectangle2D.Double(x, y, tam, tam));
+		    	x = x + tam;
+		    	g2.draw(new Rectangle2D.Double(x, y, tam, tam));
+		    	x = x + tam;
+	    	}
+	    	if(x == tamTab)
+	    	{
+	    		y = y + tam;
+	    		x = 0;
+	    	}
 	    }
-	    
-	    /*
-	     * 
-	     * 	Desenha Peças Pretas no Tabuleiro
-	     * 
-	     * */
-	    x = 19;
-	    y = 14;
-	    
-	    Image torreP1 = Toolkit.getDefaultToolkit().getImage("Assets\\Torre_P.png");
-	    g2.drawImage(torreP1, x, y, this);
-	    g2.finalize();
-	    x = x + tam;
-	    
-	    Image cavaloP1 = Toolkit.getDefaultToolkit().getImage("Assets\\Cavalo_P.png");
-	    g2.drawImage(cavaloP1, x, y, this);
-	    g2.finalize();
-	    x = x + tam;
-	    
-	    Image bispoP1 = Toolkit.getDefaultToolkit().getImage("Assets\\Bispo_P.png");
-	    g2.drawImage(bispoP1, x, y, this);
-	    g2.finalize();
-	    x = x + tam;
-	    
-	    Image rainhaP = Toolkit.getDefaultToolkit().getImage("Assets\\Rainha_P.png");
-	    g2.drawImage(rainhaP, x, y, this);
-	    g2.finalize();
-	    x = x + tam;
-	    
-	    Image reiP = Toolkit.getDefaultToolkit().getImage("Assets\\Rei_P.png");
-	    g2.drawImage(reiP, x, y, this);
-	    g2.finalize();
-	    x = x + tam;
-	    
-	    Image bispoP2 = Toolkit.getDefaultToolkit().getImage("Assets\\Bispo_P.png");
-	    g2.drawImage(bispoP2, x, y, this);
-	    g2.finalize();
-	    x = x + tam;
-	    
-	    Image cavaloP2 = Toolkit.getDefaultToolkit().getImage("Assets\\Cavalo_P.png");
-	    g2.drawImage(cavaloP2, x, y, this);
-	    g2.finalize();
-	    x = x + tam;
-	    
-	    Image torreP2 = Toolkit.getDefaultToolkit().getImage("Assets\\Torre_P.png");
-	    g2.drawImage(torreP2, x, y, this);
-	    g2.finalize();
-	    x = 17;	// Reajusta x
-	    y = y + tam; // Reajusta y
-	    
-	    Image peaoP1 = Toolkit.getDefaultToolkit().getImage("Assets\\Peao_P.png");
-	    g2.drawImage(peaoP1, x, y, this);
-	    g2.finalize();
-	    x = x + tam;
-	    
-	    Image peaoP2 = Toolkit.getDefaultToolkit().getImage("Assets\\Peao_P.png");
-	    g2.drawImage(peaoP2, x, y, this);
-	    g2.finalize();
-	    x = x + tam;
-	    
-	    Image peaoP3 = Toolkit.getDefaultToolkit().getImage("Assets\\Peao_P.png");
-	    g2.drawImage(peaoP3, x, y, this);
-	    g2.finalize();
-	    x = x + tam;
-	    
-	    Image peaoP4 = Toolkit.getDefaultToolkit().getImage("Assets\\Peao_P.png");
-	    g2.drawImage(peaoP4, x, y, this);
-	    g2.finalize();
-	    x = x + tam;
-	    
-	    Image peaoP5 = Toolkit.getDefaultToolkit().getImage("Assets\\Peao_P.png");
-	    g2.drawImage(peaoP5, x, y, this);
-	    g2.finalize();
-	    x = x + tam;
-	    
-	    Image peaoP6 = Toolkit.getDefaultToolkit().getImage("Assets\\Peao_P.png");
-	    g2.drawImage(peaoP6, x, y, this);
-	    g2.finalize();
-	    x = x + tam;
-	    
-	    Image peaoP7 = Toolkit.getDefaultToolkit().getImage("Assets\\Peao_P.png");
-	    g2.drawImage(peaoP7, x, y, this);
-	    g2.finalize();
-	    x = x + tam;
-	    
-	    Image peaoP8 = Toolkit.getDefaultToolkit().getImage("Assets\\Peao_P.png");
-	    g2.drawImage(peaoP8, x, y, this);
-	    g2.finalize();
-	    x = 17;
-	    y = 14;
-	    
-	    /*
-	     * 
-	     * 	Desenha peças brancas no tabuleiro
-	     * 
-	     * */
-	    y = y + tam*6;
-	    
-	    Image peaoB1 = Toolkit.getDefaultToolkit().getImage("Assets\\Peao_B.png");
-	    g2.drawImage(peaoB1, x, y, this);
-	    g2.finalize();
-	    x = x + tam;
-	    
-	    Image peaoB2 = Toolkit.getDefaultToolkit().getImage("Assets\\Peao_B.png");
-	    g2.drawImage(peaoB2, x, y, this);
-	    g2.finalize();
-	    x = x + tam;
-	    
-	    Image peaoB3 = Toolkit.getDefaultToolkit().getImage("Assets\\Peao_B.png");
-	    g2.drawImage(peaoB3, x, y, this);
-	    g2.finalize();
-	    x = x + tam;
-	    
-	    Image peaoB4 = Toolkit.getDefaultToolkit().getImage("Assets\\Peao_B.png");
-	    g2.drawImage(peaoB4, x, y, this);
-	    g2.finalize();
-	    x = x + tam;
-	    
-	    Image peaoB5 = Toolkit.getDefaultToolkit().getImage("Assets\\Peao_B.png");
-	    g2.drawImage(peaoB5, x, y, this);
-	    g2.finalize();
-	    x = x + tam;
-	    
-	    Image peaoB6 = Toolkit.getDefaultToolkit().getImage("Assets\\Peao_B.png");
-	    g2.drawImage(peaoB6, x, y, this);
-	    g2.finalize();
-	    x = x + tam;
-	    
-	    Image peaoB7 = Toolkit.getDefaultToolkit().getImage("Assets\\Peao_B.png");
-	    g2.drawImage(peaoB7, x, y, this);
-	    g2.finalize();
-	    x = x + tam;
-	    
-	    Image peaoB8 = Toolkit.getDefaultToolkit().getImage("Assets\\Peao_B.png");
-	    g2.drawImage(peaoB8, x, y, this);
-	    g2.finalize();
-	    x = 17;
-	    y = 14;
-	    y = y + tam*7;
-	    
-	    Image torreB1 = Toolkit.getDefaultToolkit().getImage("Assets\\Torre_B.png");
-	    g2.drawImage(torreB1, x, y, this);
-	    g2.finalize();
-	    x = x + tam;
-	    
-	    Image cavaloB1 = Toolkit.getDefaultToolkit().getImage("Assets\\Cavalo_B.png");
-	    g2.drawImage(cavaloB1, x, y, this);
-	    g2.finalize();
-	    x = x + tam;
-	    
-	    Image bispoB1 = Toolkit.getDefaultToolkit().getImage("Assets\\Bispo_B.png");
-	    g2.drawImage(bispoB1, x, y, this);
-	    g2.finalize();
-	    x = x + tam;
-	    
-	    Image rainhaB = Toolkit.getDefaultToolkit().getImage("Assets\\Rainha_B.png");
-	    g2.drawImage(rainhaB, x, y, this);
-	    g2.finalize();
-	    x = x + tam;
-	    
-	    Image reiB = Toolkit.getDefaultToolkit().getImage("Assets\\Rei_B.png");
-	    g2.drawImage(reiB, x, y, this);
-	    g2.finalize();
-	    x = x + tam;
-	    
-	    Image bispoB2 = Toolkit.getDefaultToolkit().getImage("Assets\\Bispo_B.png");
-	    g2.drawImage(bispoB2, x, y, this);
-	    g2.finalize();
-	    x = x + tam;
-	    
-	    Image cavaloB2 = Toolkit.getDefaultToolkit().getImage("Assets\\Cavalo_B.png");
-	    g2.drawImage(cavaloB2, x, y, this);
-	    g2.finalize();
-	    x = x + tam;
-	    
-	    Image torreB2 = Toolkit.getDefaultToolkit().getImage("Assets\\Torre_B.png");
-	    g2.drawImage(torreB2, x, y, this);
-	    g2.finalize();
-	    
-	}
+
+	    x = 14;
+	    y = 19;
+
+	    paintPeca(peca, g2);
+	    System.out.println("Chega aqui");
+	} 
 	
 	public void inicializaTabuleiro()
-	{
-		JFrame janela = new JFrame("Xadrez");
-	    janela.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-	    janela.setSize(816, 839);
-	    janela.getContentPane().add(new GUI_main());
-	    janela.setLocationRelativeTo(null);
-	    janela.setResizable(false);
-	    janela.setVisible(true);
-		System.out.println("Janela inicializada com sucesso!");
+	{	
+		GUI_janela j = new GUI_janela();
 	}
-
+	
+	public void inicializaPeca(Peca p)
+	{
+		peca = p;
+	}
+	
 }
