89fc0d370118b7b2ad84654a3702db9c055c6e2f
SaintLeonar <31524775+SaintLeonar@users.noreply.github.com>
2018-06-11 18:31:22 -0300

    Vers I3

--- 1612734_1413019/src/movimentos/Diagonal.java
+++ 1612734_1413019/src/movimentos/Diagonal.java
@@ -1,47 +1,57 @@
 package movimentos;
 
 import java.util.ArrayList;
+
 import tabuleiro.Consts;
+import tabuleiro.Tabuleiro;
+
 import vetor.*;
 
 public class Diagonal {
 	
-	public boolean DPPmove(Vet v)	//MODIFICA V NA DIREÇÃO PP
+	private static Tabuleiro tab;
+	
+	public Diagonal(Tabuleiro tab)
+	{
+		Diagonal.tab = tab;
+	}
+
+	public boolean DPPmove(Vet v)	//MODIFICA V NA DIRECAO PP
 	{
 		VetUtil.movePP(v);
-		if(v.getX() <= Consts.xyFin && v.getY() <= Consts.xyFin)
+		if(v.getX() <= Consts.xyFin && v.getY() <= Consts.xyFin && !tab.perguntaCasaPeca(v))
 			return true;
 		return false;
 	}
 
-	public boolean DNPmove(Vet v)	//MODIFICA V NA DIREÇÃO NP
+	public boolean DNPmove(Vet v)	//MODIFICA V NA DIRECAO NP
 	{
 		VetUtil.moveNP(v);
-		if(v.getX() >= Consts.xyIni && v.getY() <= Consts.xyFin)
+		if(v.getX() >= Consts.xyIni && v.getY() <= Consts.xyFin && !tab.perguntaCasaPeca(v))
 			return true;
 		
 		return false;
 	}
 
-	public boolean DNNmove(Vet v)	//MODIFICA V NA DIREÇÃO NN
+	public boolean DNNmove(Vet v)	//MODIFICA V NA DIRECAO NN
 	{
 		VetUtil.moveNN(v);
-		if(v.getX() >= Consts.xyIni && v.getY() >= Consts.xyIni)
+		if(v.getX() >= Consts.xyIni && v.getY() >= Consts.xyIni && !tab.perguntaCasaPeca(v))
 			return true;
 		
 		return false;
 	}
 
-	public boolean DPNmove(Vet v)	//MODIFICA V DIREÇÃO PN
+	public boolean DPNmove(Vet v)	//MODIFICA V DIRECAO PN
 	{
 		VetUtil.movePN(v);
-		if(v.getX() <= Consts.xyFin && v.getY() >= Consts.xyIni)
+		if(v.getX() <= Consts.xyFin && v.getY() >= Consts.xyIni && !tab.perguntaCasaPeca(v))
 			return true;
 		
 		return false;
 	}
 	
-	private boolean addIfTrue(Vet v, Vet Temp, ArrayList<Vet> AllMoves, int r)	//autoexplicativo
+	private boolean addIfTrue(Vet v, Vet Temp, ArrayList<Vet> AllMoves, int r)
 	{
 		
 		switch(r)
@@ -86,8 +96,8 @@ public class Diagonal {
 		}
 	}
 
-	public boolean movDiag(int r, Vet v, ArrayList<Vet> AllMoves) // MOVIMENTO DIAGONAL EM UMA DIREÇÃO E SENTIDO.
-	{ // r = região (direção e sentido) em relação ao eixo cartesiano
+	public boolean movDiag(int r, Vet v, ArrayList<Vet> AllMoves) // MOVIMENTO DIAGONAL EM UMA DIRECAO E SENTIDO.
+	{ // r = regiao (direcao e sentido) em relacao ao eixo cartesiano
 		Vet Temp = new Vet(v);
 		
 		while(addIfTrue(new Vet(Temp), Temp, AllMoves, r));
@@ -98,7 +108,7 @@ public class Diagonal {
 			return true;
 	}
 
-	public boolean allMovDiag(Vet v, ArrayList<Vet> AllMoves)	//RETORNA TODOS OS MOVIMENTOS VALIDOS PARA AS DIREÇÕES DIAGONAIS.
+	public boolean allMovDiag(Vet v, ArrayList<Vet> AllMoves)	//RETORNA TODOS OS MOVIMENTOS VALIDOS PARA AS DIRECOES DIAGONAIS.
 	{
 		for(int r = 1; r < 5; r++)
 			movDiag(r, v, AllMoves);
