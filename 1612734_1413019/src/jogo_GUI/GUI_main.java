57af424e102455f74f4342e4509ea51670296b73
SaintLeonar <31524775+SaintLeonar@users.noreply.github.com>
2018-06-11 18:28:47 -0300

    INT3

--- 1612734_1413019/src/jogo_GUI/GUI_main.java
+++ 1612734_1413019/src/jogo_GUI/GUI_main.java
@@ -5,26 +5,49 @@ import java.awt.Graphics;
 import java.awt.Graphics2D;
 import java.awt.Toolkit;
 import java.awt.geom.Rectangle2D;
-import java.util.ArrayList;
+
 import javax.swing.JComponent;
+
 import tabuleiro.*;
-import pecas.*;
+import vetor.Vet;
+import pecas.Peca;
+
 public class GUI_main extends JComponent{
 
+	GUI_janela j;
 	private static final int tamY = 2 * Consts.tamC;
 	private static final int tamTab = 8 * Consts.tamC;
-	private static ArrayList<Peca> pecas = new ArrayList<Peca>();
+	private static Tabuleiro tab;
+	private static Peca p = null;
 
-	public void paintPecas(Graphics2D g2)
+	private void paintPecas(Graphics2D g2)
 	{
-		for(Peca peca : pecas)
+		for(Peca peca : tab.getPecas())
 		{
-			g2.drawImage(Toolkit.getDefaultToolkit().getImage(peca.imgPeca()), peca.convCoorX(), peca.convCoorY(), this);
+			g2.drawImage(Toolkit.getDefaultToolkit().getImage(peca.imgPeca()), peca.getVet().convCoorX(), peca.getVet().convCoorY(), this);
 		    g2.finalize();
 		}
 	}
 
-	public void paint(Graphics g)
+    private void highlightPeca(Graphics2D g2)
+    {
+    	if(p!=null)
+    	{
+	    	g2.setColor(new Color(255, 255, 0, 125));
+	    	g2.draw(new Rectangle2D.Double(p.getX() * Consts.tamC, p.getY() * Consts.tamC, Consts.tamC, Consts.tamC));
+			g2.fill(new Rectangle2D.Double(p.getX() * Consts.tamC, p.getY() * Consts.tamC, Consts.tamC, Consts.tamC));
+			g2.setColor(new Color(0, 125, 125, 125));
+			for(Vet move : p.getAllMoves())
+			{
+				g2.draw(new Rectangle2D.Double(move.getX() * Consts.tamC, move.getY() * Consts.tamC, Consts.tamC, Consts.tamC));
+				g2.fill(new Rectangle2D.Double(move.getX() * Consts.tamC, move.getY() * Consts.tamC, Consts.tamC, Consts.tamC));
+			}
+		    g2.finalize();
+    	}
+    }
+
+    @Override
+    public void paint(Graphics g)
 	{
 	    Graphics2D g2 = (Graphics2D) g;
 	    g2.setPaint(Color.black);
@@ -57,16 +80,27 @@ public class GUI_main extends JComponent{
 	    }
 	    
 	    paintPecas(g2);
-	} 	
-	
+
+	    highlightPeca(g2);
+	    
+	    System.out.println("testeMax");
+
+	} 
+
 	public void inicializaTabuleiro(Tabuleiro tab)
 	{
-		GUI_janela j = new GUI_janela();
-	    pecas = tab.getPecas();
+		GUI_main.tab = tab;
+		j = new GUI_janela(this, tab);
+	}
+
+	public void selecPeca(Peca p)
+	{
+		GUI_main.p = p;
+		repaint();
 	}
 	
-	public void atualizaTab()
+	public void movPeca(Peca p, int x, int y)
 	{
-	
+		
 	}
 }
