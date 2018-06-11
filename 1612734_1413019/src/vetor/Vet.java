909369aaa1500204b1e159dd5b756645e9a2f136
SaintLeonar <31524775+SaintLeonar@users.noreply.github.com>
2018-06-11 18:40:19 -0300

    Vers I3

--- 1612734_1413019/src/vetor/Vet.java
+++ 1612734_1413019/src/vetor/Vet.java
@@ -1,5 +1,7 @@
 package vetor;
 
+import tabuleiro.Consts;
+
 public class Vet {
 
 	private int[] v;
@@ -12,28 +14,28 @@ public class Vet {
 		v = new int[2];
 		this.set(x,  y);
 	}
-	
+
 	public Vet(Vet v){
 		this.v = new int[2];
 		this.set(v.getX(), v.getY());
 	}
-	
+
 	public int getX(){
 		return v[0];
 	}
-	
+
 	public void setX(int x){
 		this.v[0] = x;
 	}
-	
+
 	public int getY(){
 		return v[1];
 	}
-	
+
 	public void setY(int y) {
 		this.v[1] = y;
 	}
-	
+
 	public void set(int x, int y){
 		this.v[0] = x;
 		this.v[1] = y;
@@ -59,4 +61,16 @@ public class Vet {
 		this.set(this.getX() + v.getX() , this.getY() + v.getY());
 	}
 
+	public int convCoorX()
+	{ //converte as coordenadas cartesianas em coordenada User Space
+		int coordenadaX = (this.getX() * Consts.tamC) + Consts.ajuste;
+		return coordenadaX;
+	}
+	
+	public int convCoorY()
+	{ //converte as coordenadas cartesianas em coordenada User Space
+		int coordenadaY =  (this.getY() * Consts.tamC) + Consts.ajuste;
+		return coordenadaY;
+	}
+	
 }
