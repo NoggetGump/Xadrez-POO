75422e93727b448ac325bd5d1d15156a4c7be205
SaintLeonar <31524775+SaintLeonar@users.noreply.github.com>
2018-06-13 18:30:54 -0300

    Branch atualizada

--- 1612734_1413019/src/pecas/Cavalo.java
+++ 1612734_1413019/src/pecas/Cavalo.java
@@ -6,23 +6,23 @@ import tabuleiro.Tabuleiro;
 import vetor.Vet;
 
 public class Cavalo extends Peca{
-	
+
 	/**
 	 * 
 	 *	Construtores de Peca
 	 *
 	 * */
-	
+
 	public Cavalo(int x, int y, char cor){super(x, y, cor);}
-	
+
 	public Cavalo(Vet v, char cor){super(v, cor);}
-	
+
 	/**
 	 * 
 	 *	Retorna o path da imagem
 	 *
 	 * */
-	
+
 	public String imgPeca()
 	{
 		if(this.cor == 'p')
@@ -30,15 +30,25 @@ public class Cavalo extends Peca{
 		else
 			return Consts.cavaloB.getPath();
 	}
-	
+
 	public String nomePeca()
 	{
-		return "Cavalo";
+		if(this.getCor() == 'b')
+		{
+			return "Cavalo Branco";
+		}
+		else
+		{
+			return "Cavalo Preto";
+		}
 	}
-	
+
 	public void AtualizaMoves(Tabuleiro tab)
 	{
 		Lmove lmove = new Lmove(tab);
+		
+		AllMoves.clear();
+		
 		lmove.Lmoves(this.v, AllMoves);
 	}
 }
