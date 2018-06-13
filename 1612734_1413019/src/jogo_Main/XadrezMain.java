f3fbe217266cc4145eb7056e9a9f4375cc694f9d
SaintLeonar <31524775+SaintLeonar@users.noreply.github.com>
2018-06-13 18:22:21 -0300

    Branch atualizada

--- 1612734_1413019/src/jogo_Main/XadrezMain.java
+++ 1612734_1413019/src/jogo_Main/XadrezMain.java
@@ -12,25 +12,5 @@ public class XadrezMain {
 		Tabuleiro tab= new Tabuleiro();
 		GUI_main g = new GUI_main();
 		g.inicializaTabuleiro(tab);
-
-/*		Bispo wb = new Bispo(2,3,'b');
-		Torre wt = new Torre(0,0,'b');
-		Rei wk = new Rei(4,0, 'b');
-		Rainha wq = new Rainha(3,0, 'b');
-		Peao wp = new Peao(2, 1, 'b');
-		Cavalo wh = new Cavalo(1, 0, 'b');
-
-		System.out.println("White Bishop:");
-		wb.printAllMov();
-		System.out.println("\nWhite Tower:");
-		wt.printAllMov();
-		System.out.println("\nWite Queen:");
-		wq.printAllMov();
-		System.out.println("\nWhite King:");
-		wk.printAllMov();
-		System.out.println("\nWhite Horse");
-		wh.printAllMov();
-		System.out.println("\nWhite Pawn"); 
-		wp.printAllMov();*/
 	}
 }
