49c969e47f6f7272285afee86be7f2ae545f1e49
SaintLeonar <31524775+SaintLeonar@users.noreply.github.com>
2018-06-13 18:23:18 -0300

    Branch atualizada

--- 1612734_1413019/src/movimentos/Horizontal.java
+++ 1612734_1413019/src/movimentos/Horizontal.java
@@ -8,74 +8,68 @@ import vetor.*;
 public class Horizontal {
 
 	private static Tabuleiro tab;
-	
-	public Horizontal(Tabuleiro tab)
+	private Vet coorPeca;
+
+	public Horizontal(Tabuleiro tab, Vet v)
 	{
 		Horizontal.tab = tab;
-	}
-	
-	public boolean hUnitP(Vet v) //ATENÃ‡ÃƒO, MODIFICA V!!!
-	{ //RETORNA TRUE SE O MOVIMENTO UNITARIO POSITIVO Ã‰ POSSÃ�VEL
-		v.addX(1);
-		if(v.getX() <= Consts.xyFin && !tab.perguntaCasaPeca(v))
-			return true;
-		
-		return false;
+		this.coorPeca = v;
 	}
 
-	public boolean hUnitN(Vet v)	// ATENÃ‡ÃƒO, MODIFICA V!!!
-	{ // RETORNA TRUE SE O MOVIMENTO UNITARIO NEGATIVO Ã‰ POSSÃ�VEL
-		v.addX(-1);
-		if(v.getX() >= Consts.xyIni && !tab.perguntaCasaPeca(v))
-			return true;
-		
-		return false;
-	}
-	
-	public boolean addIfTrue(Vet v, Vet Temp, ArrayList<Vet> AllMoves, boolean r) //autoexplicativo.
-	{ //r = true, se movimento for positivo
-		if(r)
+	public boolean addIfTrue(Vet v, Vet Temp, ArrayList<Vet> AllMoves, ArrayList<Vet> comiveis, boolean sentido)
+	{ //sentido = true, se movimento for positivo
+		if(sentido)
 		{
-			if(hUnitP(v))
+			v.addX(1);
+			if(v.getX() <= Consts.xyFin && !tab.perguntaCasaPeca(v))
 			{
 				AllMoves.add(v);
 				if(Temp!=null)
 					Temp.addX(1);
 				return true;
 			}
+			else
+				if(v.getX() <= Consts.xyFin && tab.perguntaComivel(coorPeca, v))
+					comiveis.add(v);
 		}
 		else
-			if(hUnitN(v))
+		{
+			v.addX(-1);
+			if(v.getX() >= Consts.xyIni && !tab.perguntaCasaPeca(v))
 			{
 				AllMoves.add(v);
 				if(Temp!=null)
 					Temp.addX(-1);
 				return true;
 			}
-		
+			else
+				if(v.getX() >= Consts.xyIni && tab.perguntaComivel(coorPeca, v))
+					comiveis.add(v);
+		}
+
 		return false;
 	}
 
-	public boolean hor(Vet v, boolean r, ArrayList<Vet> AllMoves)	//RETORNA TODOS OS MOVIMENTOS POSSIVEIS PARA EIXO X POSITIVO A PARTIR DA PECA.
-	{ //r = true, se movimento for positivo. RETORNA NULL CASO NÃƒO HAJA MOVIMENTOS POSSIVEIS NO SENTIDO ESCOLHIDO.
-		Vet Temp = new Vet(v);
-		
-		while(addIfTrue(new Vet(Temp), Temp, AllMoves, r));
+	public boolean hor(ArrayList<Vet> AllMoves, ArrayList<Vet> comiveis, boolean sentido)//RETORNA TODOS OS MOVIMENTOS POSSIVEIS PARA UM SENTIDO DO EIXO X.
+	{ //sentido = true, se movimento for positivo. RETORNA NULL CASO NÃƒO HAJA MOVIMENTOS POSSIVEIS NO SENTIDO ESCOLHIDO.
+		Vet Temp = new Vet(coorPeca);
+
+		while(addIfTrue(new Vet(Temp), Temp, AllMoves, comiveis, sentido));
 		if(AllMoves.isEmpty())
 			return false;
 		else 
-			
+
 			return true;
 	}
 
-	public boolean  allHorMov(Vet v, ArrayList<Vet> AllMoves)	//RETORNA TODOS OS MOVIMENTOS POSSIVEIS PARA EIXO X A PARTIR DA PECA.
+	public boolean  allHorMov(ArrayList<Vet> AllMoves,  ArrayList<Vet> comiveis) //RETORNA TODOS OS MOVIMENTOS POSSIVEIS PARA O EIXO X.
 	{ //RETORNA FALSE CASO NÃƒO TENHA ADICIONADO NENHUM MOVIMENTO A LISTA.
-		boolean b1 = hor(v, true, AllMoves);
-		boolean b2 = hor(v, false, AllMoves);
-		
-	if (!b1 && !b2)
-		return false;
-	else
+		boolean b1 = hor(AllMoves, comiveis, true);
+		boolean b2 = hor(AllMoves, comiveis, false);
+
+	if (b1 && b2)
 		return true;
+
+	return false;
 	}
 }
