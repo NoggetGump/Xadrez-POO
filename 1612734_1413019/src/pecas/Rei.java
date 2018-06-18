403aa2acc17cd190d83e8ec3df59e288c83fd846
SaintLeonar <31524775+SaintLeonar@users.noreply.github.com>
2018-06-18 20:06:28 -0300

    atualizado 18/06

--- 1612734_1413019/src/pecas/Rei.java
+++ 1612734_1413019/src/pecas/Rei.java
@@ -2,6 +2,7 @@ package pecas;
 
 import movimentos.Diagonal;
 import movimentos.Horizontal;
+import movimentos.Roque;
 import movimentos.Vertical;
 import tabuleiro.Consts;
 import tabuleiro.Tabuleiro;
@@ -9,23 +10,24 @@ import vetor.Vet;
 
 public class Rei extends Peca
 {
-	
+	boolean jaMoveu = false;
+
 	/**
 	 * 
 	 *	Construtores de Peca
 	 *
 	 * */
-	
+
 	public Rei(int x, int y, char cor){super(x, y, cor);}
-	
+
 	public Rei(Vet v, char cor){super(v, cor);}
-	
+
 	/**
 	 * 
 	 *	Retorna o path da imagem
 	 *
 	 * */
-	
+
 	public String imgPeca()
 	{
 		if(this.cor == 'p')
@@ -33,17 +35,23 @@ public class Rei extends Peca
 		else
 			return Consts.reiB.getPath();
 	}
-	
+
 	public String nomePeca()
 	{
-		if(this.getCor() == 'b')
-		{
+		if(this.cor == 'b')
 			return "Rei Branco";
-		}
 		else
-		{
 			return "Rei Preto";
-		}
+	}
+
+	public boolean getJaMoveu()
+	{
+		return jaMoveu;
+	}
+
+	public void toogleJaMoveu()
+	{
+		jaMoveu = !jaMoveu;
 	}
 
 	public void AtualizaMoves(Tabuleiro tab)
@@ -51,7 +59,9 @@ public class Rei extends Peca
 		Vertical V = new Vertical(tab, this.v);
 		Horizontal H = new Horizontal(tab, this.v);
 		Diagonal D = new Diagonal(tab, this.v);
-		
+		Roque R = new Roque(tab, this.v);
+		Vet temp = new Vet();
+
 		AllMoves.clear();
 		comiveis.clear();
 
@@ -63,5 +73,21 @@ public class Rei extends Peca
 		D.DNPmove(new Vet(this.v), AllMoves, comiveis);
 		D.DPNmove(new Vet(this.v), AllMoves, comiveis);
 		D.DNNmove(new Vet(this.v), AllMoves, comiveis);
+
+		if(this.corP())
+		{
+			temp.set(Consts.xyFin, Consts.xyIni);
+			R.roqueEsquerdaP(this, temp, AllMoves);
+			temp.set(Consts.xyIni, Consts.xyIni);
+			R.roqueDireitaP(this, temp, AllMoves);
+		}
+		else
+		{
+			temp.set(Consts.xyIni, Consts.xyFin);
+			R.roqueEsquerdaB(this, temp, AllMoves);
+			temp.set(Consts.xyFin, Consts.xyFin);
+			R.roqueDireitaB(this, temp, AllMoves);
+			this.printAllMov();
+		}
 	}
 }
