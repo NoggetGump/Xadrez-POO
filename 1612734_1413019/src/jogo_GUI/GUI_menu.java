a093f31311d45bf0c21d294931e6ece00da9d26d
SaintLeonar <31524775+SaintLeonar@users.noreply.github.com>
2018-06-18 19:57:46 -0300

    GUI_menu para popupMenu promo, popupMenu Save e menu principal

---
+++ 1612734_1413019/src/jogo_GUI/GUI_menu.java
@@ -0,0 +1,16 @@
+package jogo_GUI;
+
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+
+import javax.swing.JButton;
+import javax.swing.JMenuItem;
+import javax.swing.JPopupMenu;
+
+public class GUI_menu {
+
+	public void popupPromo()
+	{
+		
+	}
+}
