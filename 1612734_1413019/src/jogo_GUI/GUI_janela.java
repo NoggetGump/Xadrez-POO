655c6350e7380d2a289d4043c59c319647c1cf0c
SaintLeonar <31524775+SaintLeonar@users.noreply.github.com>
2018-06-23 13:04:24 -0300

    Salvamento de partida adicionado

--- 1612734_1413019/src/jogo_GUI/GUI_janela.java
+++ 1612734_1413019/src/jogo_GUI/GUI_janela.java
@@ -2,58 +2,88 @@ package jogo_GUI;
 
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
+import java.io.File;
 
 import javax.swing.ImageIcon;
+import javax.swing.JFileChooser;
 import javax.swing.JFrame;
+import javax.swing.SwingUtilities;
 
 import pecas.Peca;
 import tabuleiro.Consts;
 import tabuleiro.Tabuleiro;
 
 public class GUI_janela extends JFrame {
-	
+
 	Peca pecaSelecionada;
+	boolean logicAssist = false;
 	int turno = 1;
+	JFileChooser fc = new JFileChooser();
 	
 		public GUI_janela(GUI_main a, Tabuleiro tab)
 		{
-			
+			fc.setCurrentDirectory(new java.io.File("saves"));
+			fc.setDialogTitle("Salvar Jogo - ESCREVA O NOME DO ARQUIVO DE SALVAMENTO");
+			fc.setFileSelectionMode(JFileChooser.DIRECTORIES_ONLY);
+
 			addMouseListener(new MouseAdapter() 
 			{
 				public void mouseClicked(MouseEvent e)
 				{
-					int x = (e.getX()-4)/100;
-					int y = (e.getY()-26)/100;
-					Peca temp = tab.buscaPeca(x, y);
-					if(temp != null && temp.turno(turno)) // Se houver peca na Casa clicada e se o turno do jogador for respeitada
+					// Mouse Esquerdo
+					if(SwingUtilities.isLeftMouseButton(e))
 					{
-						pecaSelecionada = temp;
-						a.selecPeca(pecaSelecionada);
-					}
-					else
-					if((temp == null || temp != pecaSelecionada) && pecaSelecionada != null)
-					{
-						if(temp == null)
+						int x = (e.getX()-4)/100;
+						int y = (e.getY()-26)/100;
+						Peca temp = tab.buscaPeca(x, y);
+						
+						if(temp != pecaSelecionada
+						&& pecaSelecionada != null)
 						{
-							if(a.movPeca(pecaSelecionada, x, y))
+							if(temp == null)
 							{
-								turno ++;
-								System.out.println("\r\n<< Turno " + turno + " >>\r\n");
-							}		
+								if(a.movPeca(pecaSelecionada, x, y))
+								{
+									turno ++;
+									System.out.println("\r\n<< Turno " + turno + " >>\r\n");
+								}
+							}
+							else
+							{
+								if(a.comePecaOuRoque(pecaSelecionada, temp))
+								{
+									turno++;
+									System.out.println("\r\n<< Turno " + turno + " >>\r\n");
+								}
+							}
+							pecaSelecionada = null;
 						}
 						else
-						{
-							if(a.comePeca(pecaSelecionada, temp))
+							if(temp != null
+							&& temp.turno(turno)) // Se houver peca na Casa clicada e se o turno do jogador for respeitado
 							{
-								turno++;
-								System.out.println("\r\n<< Turno " + turno + " >>\r\n");
+								pecaSelecionada = temp;
+								a.selecPeca(pecaSelecionada);
 							}
-						}
-					pecaSelecionada = null;
 					}
+					else
+						// Mouse Direito
+						if(SwingUtilities.isRightMouseButton(e))
+						{
+							int returnval = fc.showOpenDialog(getParent());
+							if(returnval == JFileChooser.APPROVE_OPTION)
+							{
+								tab.salvaPartida(fc.getSelectedFile() + ".txt");	
+							}
+							else
+							{
+								System.out.println("Salvamento de partida cancelado pelo usuário");
+							}
+							
+						}
 				}
 			});
-			
+
 			ImageIcon img = new ImageIcon("Assets\\Chess_Icon2.png");
 			this.setIconImage(img.getImage());
 			this.setTitle("Xadrez");
