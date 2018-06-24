d904834a6e987ffb71dcf19c0a72aa22e85dfe95
SaintLeonar <31524775+SaintLeonar@users.noreply.github.com>
2018-06-24 17:59:35 -0300

    Update GUI_OptionPane.java

--- 1612734_1413019/src/jogo_GUI/GUI_OptionPane.java
+++ 1612734_1413019/src/jogo_GUI/GUI_OptionPane.java
@@ -5,13 +5,47 @@ import javax.swing.JOptionPane;
 
 public class GUI_OptionPane {
 	
+	/**
+	 * 
+	 * 	Menu de inicialização do jogo
+	 * 
+	 * */
 	public int loadGame ()
 	{
 		int resposta;
 		ImageIcon img = new ImageIcon("Assets\\Chess_Icon2.png");
 		
 		resposta = JOptionPane.showConfirmDialog(null,"Deseja carregar um jogo salvo?", "Xadrez", 0, 0, img);
+		System.out.println(resposta);
 		
 		return resposta;
+	}
+	
+	/**
+	 * 
+	 * 	Menu de Game Over
+	 * 
+	 * */
+	public void gameOver (char vencedor)
+	{
+		ImageIcon img;
+		
+		switch(vencedor)
+		{
+		case 'b':
+		{
+			img = new ImageIcon("Assets\\Rainha_B.png");
+			JOptionPane.showMessageDialog(null, "Vencedor:  jogador branco!", "Xeque-Mate", 0, img);
+			break;
+		}
+		case 'p':
+		{
+			img = new ImageIcon("Assets\\Rainha_P.png");
+			JOptionPane.showMessageDialog(null, "Vencedor:  jogador preto!", "Xeque-Mate", 0, img);
+			break;
+		}
+		default:
+			break;
+		}
 	}
 }
