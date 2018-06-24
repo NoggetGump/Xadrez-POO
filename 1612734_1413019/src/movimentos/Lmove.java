b05cd453af661f8ebdffbd4d9012e9a646c7a03a
SaintLeonar <31524775+SaintLeonar@users.noreply.github.com>
2018-06-24 00:37:57 -0300

    Update Lmove.java

--- 1612734_1413019/src/movimentos/Lmove.java
+++ 1612734_1413019/src/movimentos/Lmove.java
@@ -8,15 +8,17 @@ import vetor.*;
 public class Lmove {
 
 	private static Tabuleiro tab;
-	
-	public Lmove(Tabuleiro tab)
+	private Vet coorPeca;
+
+	public Lmove(Tabuleiro tab, Vet coorPeca)
 	{
 		Lmove.tab = tab;
+		this.coorPeca = coorPeca;
 	}
-	
-	public boolean L1(Vet v, ArrayList<Vet> AllMoves, int Mx, int My) //
+
+	public void L1(ArrayList<Vet> AllMoves, ArrayList<Vet> comiveis, int Mx, int My) //
 	{
-		Vet Temp = new Vet(v);
+		Vet Temp = new Vet(coorPeca);
 
 		Temp.add(Mx, 2*My);
 		if(Temp.getX() <= Consts.xyFin
@@ -24,16 +26,19 @@ public class Lmove {
 		&& Temp.getY() <= Consts.xyFin
 		&& Temp.getY() >= Consts.xyIni
 		&& !tab.perguntaCasaPeca(Temp))
-		{
 			AllMoves.add(Temp);
-			return true;
-		}
-		return false;
+		else
+			if(Temp.getX() <= Consts.xyFin
+			&& Temp.getX() >= Consts.xyIni
+			&& Temp.getY() <= Consts.xyFin
+			&& Temp.getY() >= Consts.xyIni
+			&& tab.perguntaComivel(Temp, coorPeca))
+				comiveis.add(Temp);
 	}
-	
-	public boolean L2(Vet v, ArrayList<Vet> AllMoves, int Mx, int My) //
+
+	public void L2(ArrayList<Vet> AllMoves, ArrayList<Vet> comiveis, int Mx, int My) //
 	{
-		Vet Temp = new Vet(v);
+		Vet Temp = new Vet(coorPeca);
 
 		Temp.add(2*Mx, My);
 		if(Temp.getX() <= Consts.xyFin
@@ -41,23 +46,26 @@ public class Lmove {
 		&& Temp.getY() <= Consts.xyFin
 		&& Temp.getY() >= Consts.xyIni
 		&& !tab.perguntaCasaPeca(Temp))
-		{
 			AllMoves.add(Temp);
-			return true;
-		}
-		return false;
+		else
+			if(Temp.getX() <= Consts.xyFin
+			&& Temp.getX() >= Consts.xyIni
+			&& Temp.getY() <= Consts.xyFin
+			&& Temp.getY() >= Consts.xyIni
+			&& tab.perguntaComivel(Temp, coorPeca))
+				comiveis.add(Temp);
 	}
 
-	public boolean Lmoves(Vet v, ArrayList<Vet> AllMoves) //TODOS OS MOVIMENTOS EM "L" - CAVALO
+	public boolean Lmoves(ArrayList<Vet> AllMoves, ArrayList<Vet> comiveis) //TODOS OS MOVIMENTOS EM "L" - CAVALO
 	{
 		int Mx = 1;
 		int My = 1;
 		for(int i = 0; i < 2; i++)
 		{
-			L1(v, AllMoves, Mx, My);
-			L1(v, AllMoves, Mx, -My);
-			L2(v, AllMoves, Mx, My);
-			L2(v, AllMoves, Mx, -My);
+			L1(AllMoves, comiveis, Mx, My);
+			L1(AllMoves, comiveis, Mx, -My);
+			L2(AllMoves, comiveis, Mx, My);
+			L2(AllMoves, comiveis, Mx, -My);
 			Mx *= -1;
 			My *= -1;
 		}
